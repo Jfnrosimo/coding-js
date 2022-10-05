6c85fa768d1cde1398610d2789c5772330d9c42b
jrosi <j.rosimo098@gmail.com>
2022-10-05 22:27:03 +0800

    Feat: added html content and class book in js

---
+++ book-list/script.js
@@ -0,0 +1,15 @@
+class Book {
+    constructor (title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+class AddBook {
+    
+    addBookToList(book) {
+        
+        const list = document.getElementById('book-list')
+    }
+}
